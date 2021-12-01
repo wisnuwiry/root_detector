3cbe36bcd43c1511f583505715b941dd7b3dc31f
Wisnu Ginanjar Saputra <wisnuwiry@gmail.com>
2021-12-01 20:49:36 +0700

    feat: ✨ Implement Root Detector in iOS platform

--- ios/Classes/SwiftRootDetectorPlugin.swift
+++ ios/Classes/SwiftRootDetectorPlugin.swift
@@ -2,13 +2,27 @@ import Flutter
 import UIKit
 
 public class SwiftRootDetectorPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "root_detector", binaryMessenger: registrar.messenger())
-    let instance = SwiftRootDetectorPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "space.wisnuwiry/root_detector", binaryMessenger: registrar.messenger())
+        let instance = SwiftRootDetectorPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if(call.method == "checkIsRooted"){
+            let isJailBroken = UIDevice.current.isJailBroken
+            let isSimulator = UIDevice.current.isSimulator
+            let args = call.arguments
+            
+            if let ignoreSimulator = args as? Bool {
+                if ignoreSimulator && isSimulator && isJailBroken {
+                    result(false)
+                }else{
+                    result(isJailBroken)
+                }
+            }else{
+                result(isJailBroken)
+            }
+        }
+    }
 }
